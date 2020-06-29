3a91bf6aa03b267a0ad30e499180bb966d085aa1
Bhagavan_1999 <44443781+Bhagava@users.noreply.github.com>
2020-06-29 22:01:42 +0530

    Update Files.java

--- src/interdemo/file/Files.java
+++ src/interdemo/file/Files.java
@@ -4,7 +4,6 @@ import exceptions.pack.FileNotFoundException;
 import exceptions.pack.SearchFileNotFoundException;
 
 public interface Files {
-	public void sortfiles();
 	public void addfile();
 	public void deletefile() throws FileNotFoundException;
 	public void searchfile() throws SearchFileNotFoundException;
